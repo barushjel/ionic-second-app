a9c0602c6e7d01b4ce886a5b66563d3b79a557a4
Espinosa <Barush.Espinosa@ellucian.com>
2021-02-20 21:09:33 -0600

    Fin Router Link

---
+++ src/app/components/menu/menu.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent implements OnInit {
+
+  routeBe = [
+    {
+      name : 'Home Page',
+      path: '/home' 
+    },
+    {
+      name : 'About Page',
+      path: '/about' 
+    },
+    {
+      name : 'Contact Page',
+      path: '/contact' 
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
